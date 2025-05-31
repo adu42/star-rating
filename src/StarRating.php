8523738854eb340e4d5b9724229ad787a568b26d
ado <adu42@users.noreply.github.com>
2025-05-31 18:48:10 +0800

    Update StarRating.php

--- src/StarRating.php
+++ src/StarRating.php
@@ -6,7 +6,7 @@
  * Time: 16:39
  */
 
-namespace Encore\StarRating;
+namespace Adu42\StarRating;
 
 
 use Encore\Admin\Admin;
@@ -17,17 +17,17 @@ class StarRating extends Field
     protected $view = 'laravel-admin-star-rating::input';
 
     protected static $css = [
-        'vendor/laravel-admin-ext/star-rating/bootstrap-star-rating-4.0.3/css/star-rating.min.css',
+        'vendor/adu42/star-rating/bootstrap-star-rating-4.0.3/css/star-rating.min.css',
     ];
     protected static $js = [
-        'vendor/laravel-admin-ext/star-rating/bootstrap-star-rating-4.0.3/js/star-rating.min.js',
+        'vendor/adu42/star-rating/bootstrap-star-rating-4.0.3/js/star-rating.min.js',
     ];
 
     public function render()
     {
         $config = config('admin.extensions.star-rating.config');
         if (isset($config['language']) && file_exists(__DIR__ . '/../resources/assets/bootstrap-star-rating-4.0.3/js/locales/' . $config['language'] . '.js')) {
-            Admin::js('vendor/laravel-admin-ext/star-rating/bootstrap-star-rating-4.0.3/js/locales/zh.js');
+            Admin::js('vendor/adu42/star-rating/bootstrap-star-rating-4.0.3/js/locales/zh.js');
         }
         $config = json_encode(config('admin.extensions.star-rating.config'));
         $config = $config ? $config : '';
